cd25c83f5ca468c92a2cd13526fddb555049e4b0
hbassole <hermann.bassole@veone.net>
2024-05-13 15:07:03 +0000

    fix content string null safety

--- android/src/main/kotlin/com/kd/custom_social_share/CustomSocialSharePlugin.kt
+++ android/src/main/kotlin/com/kd/custom_social_share/CustomSocialSharePlugin.kt
@@ -65,7 +65,7 @@ class CustomSocialSharePlugin : FlutterPlugin, MethodCallHandler, ActivityAware 
     override fun onDetachedFromActivity() {}
 
     override fun onMethodCall(call: MethodCall, result: Result) {
-        val content: String = call.argument("content")
+        val content: String? = call.argument("content")
         when (call.method) {
 
             "sms" -> {
